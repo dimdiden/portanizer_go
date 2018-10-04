9eb78d722885ea8553ce80e1d1d51daa60958c9c
Your Name <dimdiden@gmail.com>
2018-10-05 00:18:29 +0300

    Changed notification style

--- cmd/portanizer/main.go
+++ cmd/portanizer/main.go
@@ -18,13 +18,14 @@ var (
 func main() {
 	// Load the configuration either from environment or from the default values
 	c := NewConf()
-	fmt.Print("running configuration:\n", c)
+	fmt.Println("[[> configurator initiated...")
+	fmt.Print(c)
 
-	db, err := c.OpenDB()
+	db, err := c.OpenGormDB()
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("database connection has been established")
+	fmt.Println("[[> database connection has been established...")
 	defer db.Close()
 
 	// Migrate any changed in structs to DB schema
@@ -34,5 +35,6 @@ func main() {
 	// Init server, enable logs and run it
 	s := server.New(gorm.NewPostRepo(db), gorm.NewTagRepo(db))
 	s.LogEnable()
+	fmt.Printf("[[> listening on %v port...", c.APPport)
 	log.Fatal(http.ListenAndServe(":"+c.APPport, s))
 }
