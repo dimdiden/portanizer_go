fcff0c87673e20524c721697f0071035841c2700
Your Name <dimdiden@gmail.com>
2018-10-25 16:23:09 +0300

    Removed unnecessary strucure

--- server/json.go
+++ server/json.go
@@ -5,17 +5,13 @@ import (
 	"net/http"
 )
 
-type Info struct {
-	Message string
-}
-
 func renderJSON(w http.ResponseWriter, data interface{}, code int) {
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	w.WriteHeader(code)
 
 	switch data := data.(type) {
 	case string:
-		info := &Info{Message: data}
+		info := struct{ Message string }{data}
 		res, err := json.Marshal(info)
 		if err != nil {
 			renderJSON(w, "Can not marshal output", http.StatusInternalServerError)
